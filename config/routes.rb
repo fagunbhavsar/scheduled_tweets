3897d0be3fa4be7cd7096c635ca543f956bbc2f4
Fagun Bhavsar <apple@Apples-MacBook-Pro.local>
2021-02-12 18:19:05 +0530

    Initia commit for project scheduled_tweets

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # GET /about
+  get "about", to: "about#index"
+
+  #For details on the DSL availabe within this file, see https://guides.rubyonrails.org/routing.html
+
+end
